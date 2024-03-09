0c6294bc70c154d52bbb4a7af2fd512b83967ebb
Nicolas <nico281@hotmail.com>
2024-03-09 02:12:14 -0300

    change conform

--- lua/custom/plugins/autotag.lua
+++ lua/custom/plugins/autotag.lua
@@ -5,4 +5,3 @@ return {
     autotag.setup {}
   end,
 }
-
--- lua/custom/plugins/conform.lua
+++ lua/custom/plugins/conform.lua
@@ -1,34 +1,33 @@
 return {
-    'stevearc/conform.nvim',
-    event = { 'BufReadPre', 'BufNewFile' },
-    opts = {
-      notify_on_error = false,
-      format_on_save = {
-        timeout_ms = 500,
-        lsp_fallback = true,
-      },
+  'stevearc/conform.nvim',
+  event = { 'BufReadPre', 'BufNewFile' },
+  config = function()
+    local conform = require 'conform'
+    conform.setup {
       formatters_by_ft = {
+        javascript = { 'prettier' },
+        typescript = { 'prettier' },
+        typescriptreact = { 'prettier' },
+        javascriptreact = { 'prettier' },
+        css = { 'prettier' },
+        scss = { 'prettier' },
+        html = { 'prettier' },
+        json = { 'prettier' },
+        astro = { 'prettier' },
         lua = { 'stylua' },
-        javascript = { 'prettier', 'prettierd' },
-        typescript = { 'prettier', 'prettierd'},
-        typescriptreact = { 'prettier', 'prettierd'},
-        javascriptreact = { 'prettier', 'prettierd'},
-        css = { 'prettier', 'prettierd' },
-        scss = { 'prettier', 'prettierd' },
-        html = { 'prettier', 'prettierd' },
-        json = { 'prettier', 'prettierd'},
-        astro = { 'prettier', 'prettierd'},
-        format_on_save = {
-          lsp_fallback = true,
-          async = false,
-          timeout_ms = 500,
-        },
-        -- Conform can also run multiple formatters sequentially
-        -- python = { "isort", "black" },
-        --
-        -- You can use a sub-list to tell conform to run *until* a formatter
-        -- is found.
-        -- javascript = { { "prettierd", "prettier" } },
       },
-    },
-  }
+      format_on_save = {
+        lsp_fallback = true,
+        async = false,
+        timeout_ms = 500,
+      },
+    }
+    vim.keymap.set({ 'n', 'v' }, '<leader>cf', function()
+      conform.format {
+        lsp_fallback = true,
+        async = false,
+        timeout_ms = 500,
+      }
+    end, { desc = '[C]ode [F]ormat' })
+  end,
+}
